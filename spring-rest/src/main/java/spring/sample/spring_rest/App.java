5a2b158e9c83a2bfd840ddae11abc004e4697285
Sushanta <susha@DESKTOP-29LD4TG>
2018-04-09 23:48:17 +0100

    initial commit

---
+++ spring-rest/src/main/java/spring/sample/spring_rest/App.java
@@ -0,0 +1,13 @@
+package spring.sample.spring_rest;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
